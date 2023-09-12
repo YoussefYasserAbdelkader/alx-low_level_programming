29af61fba1d9cab8a3d9a5d7b5dee7f5b29cad11
YoussefYasserAbdelkader <youssefyasserabdelkader@gmail.com>
2023-09-12 06:52:46 -0700

    alpahabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include<main.h>
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+for (char i = 'a'; i <= 'z'; i++)
+{
+-putchar(i);
+-putchar('\n');
+}
+}
