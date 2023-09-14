637760382fd36e99b72aa792c6b820b07565c8fa
YoussefYasserAbdelkader <youssefyasserabdelkader@gmail.com>
2023-09-14 07:17:43 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print num except 2 and 4
+ *
+ * return: void
+ */
+#include <stdio.h>
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		if (!(x == 50 || x == 52))
+			_putchar(x);
+	}
+	_putchar('\n');
+}
