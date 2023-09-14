dc04175fdabdf9eac5f6c3a543bfc04a6b3ed42c
YoussefYasserAbdelkader <youssefyasserabdelkader@gmail.com>
2023-09-14 06:43:06 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*Upper case letters return 1
+*else return 0
+**/
+#include <stdio.h>
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
